b931b099e9dac7d0e4f84f37247ef1b27e8af01f
Karina <sevynidd02@protonmail.com>
2026-07-11 19:58:31 +0200

    Changed Version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 31
         targetSdk = 37
         versionCode = 1
-        versionName = "1.7"
+        versionName = "1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
